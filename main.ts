43725db3b4e69fadd6edbff304387fa163902fd6
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-03-21 12:36:06 -0500

    upgrade TypeScript to v5.0.2

--- main.ts
+++ main.ts
@@ -1,1 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-unused-vars
+type Resolve<T> = T extends Function ? T : { [K in keyof T]: T[K] };
+
 console.log('Hello, world!');
